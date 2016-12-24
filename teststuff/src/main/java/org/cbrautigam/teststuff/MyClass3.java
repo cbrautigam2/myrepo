838b46f2061945181d06dd1b73d7e3b1a8a30b56
cbrautigam2 <craig_80012@yahoo.com>
2016-12-24 04:30:31 -0700

    mine

--- teststuff/src/main/java/org/cbrautigam/teststuff/MyClass3.java
+++ teststuff/src/main/java/org/cbrautigam/teststuff/MyClass3.java
@@ -2,7 +2,7 @@ package org.cbrautigam.teststuff;
 
 public class MyClass3 {
 	public static void main(String[] args) {
-		System.out.println("howdyagains");
+		System.out.println("howdyagainsohno");
 		System.out.println("easy");
 	}
 	
